42b1f36708722f57e792e10a9d5b9c7fd0124bb2
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-10 10:04:03 -0500

    ejerc1 insertar y actualizar con campo edad

--- src/main/java/com/clearminds/ddi/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/ddi/servicios/RestEstudiantes.java
@@ -1,20 +1,22 @@
 package com.clearminds.ddi.servicios;
 
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.ddi.dtos.Estudiante;
 import com.clearminds.ddi.excepciones.BDDException;
 import com.clearminds.servicios.ServicioEstudiante;
+
 @Path("/estudiantes")
 public class RestEstudiantes {
 	@Path("/insertar")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void insertar(Estudiante estudiante){
+	public void insertar(Estudiante estudiante) {
 		estudiante.toString();
 		ServicioEstudiante servicio = new ServicioEstudiante();
 		try {
@@ -23,6 +25,22 @@ public class RestEstudiantes {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Path("/actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Estudiante actualizar(Estudiante estudiante) {
+		estudiante.toString();
+		ServicioEstudiante servicio = new ServicioEstudiante();
+		try {
+
+			servicio.actualizarEstudiante(estudiante);
+
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
+		return estudiante;
+	}
 
 }
